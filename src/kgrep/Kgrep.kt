d8583079c6379865a5cb219b6961642dfdb2c1c4
Domenico Fabio Marino <johnlemon.lol@gmail.com>
2015-11-25 23:54:50 +0000

    Kgrep: clean up

--- src/kgrep/Kgrep.kt
+++ src/kgrep/Kgrep.kt
@@ -3,21 +3,21 @@
  */
 package kgrep
 
-import java.io.File
-import java.util.concurrent.Executors
-import kotlin.concurrent.thread
-
 val sharedThreadPool = Executors.newCachedThreadPool()
 
 fun main(args: Array<String>) {
-    try {
-        val filter = args[0]
-        // Drop args[0]
-        args.drop(1)
-        // Only search files, walk top down the tree
-        args.forEach { path -> File(path).walkTopDown().filterNot { elem -> !elem.isFile }.forEach { elem -> matches(elem, filter) } }
-    } catch(e: ArrayIndexOutOfBoundsException) {
-        println("Usage: <pattern> <files>")
+    if (args.size < 2) {
+        println("Usage: kgrep <pattern> <folder1, folder2 ..>")
+        return
+    }
+    val filter = args[0]
+    // Drop args[0]
+    args.drop(1)
+    // Only search files, walk top down the tree
+    args.forEach { path ->
+        File(path).walkTopDown()
+                .filterNot { elem -> !elem.isFile }
+                .forEach { elem -> matches(elem, filter) }
     }
 }
 
@@ -28,7 +28,7 @@ fun main(args: Array<String>) {
  */
 fun matches(file: File, filter: String) {
     sharedThreadPool.submit {
-        thread(start = true, daemon = false, contextClassLoader = null, name = "match", priority = -1, block = {
+        thread(block = {
             file.bufferedReader().lines()
                     .filter { line -> line.contains(filter) }.toArray()
                     .forEachIndexed { i, line -> println("${file.absolutePath}:${i + 1} $line") }
